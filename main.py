6c815f0be1d2857cd30be512891e7dc61bfeca44
1Ch222 <137825924+1Ch222@users.noreply.github.com>
2023-07-11 16:03:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -62,6 +62,6 @@ def convert_images_to_greyscale(input_folder, output_folder):
             image.save(output_path, "JPEG")
 
 # Exemple d'utilisation
-input_folder = "/home/poc2014/dataset/temp/INFRA10/semantic_segmentation_truth/test/Saint-Cyr/"
-output_folder = "/home/poc2014/grey_output/g_Saint-Cyr"
+input_folder = "/home/poc2014/dataset/temp/INFRA10/semantic_segmentation_truth/test/Verrieres/"
+output_folder = "/home/poc2014/grey_output/g_Verrieres"
 convert_images_to_greyscale(input_folder, output_folder)
